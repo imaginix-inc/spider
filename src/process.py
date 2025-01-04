691809eaefb03026a11e0011664699db000a9383
Dorbmon <dorbmons@gmail.com>
2025-01-04 13:56:41 +0800

    ProgessBar

--- src/process.py
+++ src/process.py
@@ -5,6 +5,7 @@ from openai.types import CreateEmbeddingResponse
 from sqlalchemy.sql import func
 import asyncio
 from snowflake import SnowflakeGenerator
+from tqdm.asyncio import tqdm
 generator = SnowflakeGenerator(1)
 client = AsyncClient()
 
@@ -14,7 +15,7 @@ async def post_process(datas: List[BaseDB], embedding_texts: List[str], full_tex
     # TODO: cache
     tasks = [client.embeddings.create(
         input=text, model='text-embedding-3-large') if text is not None else None for text in embedding_texts]
-    embeddings: List[CreateEmbeddingResponse] = await asyncio.gather(*[task for task in tasks if task is not None])
+    embeddings: List[CreateEmbeddingResponse] = await tqdm.gather(*[task for task in tasks if task is not None], desc="Post Process")
     embeddings = [
         embedding.data[0].embedding if embedding is not None else None for embedding in embeddings]
     # update datas
