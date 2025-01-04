910e044eb09887235b47a1535938b5599a53b024
Dorbmon <dorbmons@gmail.com>
2025-01-04 14:57:00 +0800

    fix

--- src/process.py
+++ src/process.py
@@ -9,15 +9,23 @@ from tqdm.asyncio import tqdm
 generator = SnowflakeGenerator(1)
 client = AsyncClient()
 
+sem = asyncio.Semaphore(50)
+
+
+async def embedd_text(text: str | None):
+    async with sem:
+        if text is None:
+            return None
+        resp = await client.embeddings.create(
+            input=text, model='text-embedding-3-large')
+        return resp.data[0].embedding
+
 
 async def post_process(datas: List[BaseDB], embedding_texts: List[str], full_texts: List[str]):
     # get embeddings of texts
     # TODO: cache
-    tasks = [client.embeddings.create(
-        input=text, model='text-embedding-3-large') if text is not None else None for text in embedding_texts]
+    tasks = [embedd_text(text) for text in embedding_texts]
     embeddings: List[CreateEmbeddingResponse] = await tqdm.gather(*[task for task in tasks if task is not None], desc="Post Process")
-    embeddings = [
-        embedding.data[0].embedding if embedding is not None else None for embedding in embeddings]
     # update datas
     for (data, embedding) in zip(datas, embeddings):
         data.name_vector = embedding
